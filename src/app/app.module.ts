3ce5e5295bcd545652316b1194b3acb2d7bb377e
Arifullah Jan <34775308+ajanbscs16seecs@users.noreply.github.com>
2018-04-06 21:35:57 +0500

    ...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@
 * this file contains app level dependencies
 */
 import { NgModule }       from '@angular/core';
+
+import {CdkTableModule} from '@angular/cdk/table';
 import { BrowserModule }  from '@angular/platform-browser';
 import { HttpClientModule }    from '@angular/common/http';
 
@@ -23,6 +25,91 @@ import {MatButtonModule} from '@angular/material';
 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
+
+
+import {
+  MatAutocompleteModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatDividerModule,
+  MatExpansionModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatSortModule,
+  MatStepperModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatSidenavModule
+} from '@angular/material';
+import {BrowserModule} from '@angular/platform-browser';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {SidenavResponsiveExample} from './app/sidenav-responsive-example';
+
+@NgModule({
+  exports: [
+    CdkTableModule,
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatStepperModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatDividerModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    MatSidenavModule
+  ]
+})
+export class DemoMaterialModule {}
+
+
+
+
 @NgModule({
   imports: [
     BrowserModule,
@@ -32,6 +119,7 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
     MatSidenavModule,
     MatButtonModule,
     BrowserAnimationsModule
+    DemoMaterialModule
 
     // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
     // and returns simulated server responses.
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,109 +1,109 @@
-import { NgModule } from '@angular/core';
-
+// import { NgModule } from '@angular/core';
+//
+// // import {
+// //   MatButtonModule,
+// //   MatMenuModule,
+// //   MatToolbarModule,
+// //   MatIconModule,
+// //   MatCardModule,
+// // } from '@angular/material';
+// //
+// // @NgModule({
+// //   imports: [
+// //     MatButtonModule,
+// //     MatMenuModule,
+// //     MatToolbarModule,
+// //     MatIconModule,
+// //     MatCardModule,
+// //     MatSidenavModule,
+// //   ],
+// //   declarations: [
+// //   ],
+// //
+// //   exports: [
+// //     MatButtonModule,
+// //     MatMenuModule,
+// //     MatToolbarModule,
+// //     MatIconModule,
+// //     MatCardModule,
+// //     MatSidenavModule
+// //   ]
+// // })
+//
+//
+// import {BrowserModule} from '@angular/platform-browser';
+// import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+// import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+//
 // import {
+//   MatAutocompleteModule,
 //   MatButtonModule,
-//   MatMenuModule,
-//   MatToolbarModule,
-//   MatIconModule,
+//   MatButtonToggleModule,
 //   MatCardModule,
+//   MatCheckboxModule,
+//   MatChipsModule,
+//   MatDatepickerModule,
+//   MatDialogModule,
+//   MatDividerModule,
+//   MatExpansionModule,
+//   MatGridListModule,
+//   MatIconModule,
+//   MatInputModule,
+//   MatListModule,
+//   MatMenuModule,
+//   MatNativeDateModule,
+//   MatPaginatorModule,
+//   MatProgressBarModule,
+//   MatProgressSpinnerModule,
+//   MatRadioModule,
+//   MatRippleModule,
+//   MatSelectModule,
+//   MatSidenavModule,
+//   MatSliderModule,
+//   MatSlideToggleModule,
+//   MatSnackBarModule,
+//   MatSortModule,
+//   MatStepperModule,
+//   MatTableModule,
+//   MatTabsModule,
+//   MatToolbarModule,
+//   MatTooltipModule,
 // } from '@angular/material';
 //
 // @NgModule({
-//   imports: [
-//     MatButtonModule,
-//     MatMenuModule,
-//     MatToolbarModule,
-//     MatIconModule,
-//     MatCardModule,
-//     MatSidenavModule,
-//   ],
-//   declarations: [
-//   ],
-//
 //   exports: [
+//     MatAutocompleteModule,
 //     MatButtonModule,
-//     MatMenuModule,
-//     MatToolbarModule,
-//     MatIconModule,
+//     MatButtonToggleModule,
 //     MatCardModule,
-//     MatSidenavModule
+//     MatCheckboxModule,
+//     MatChipsModule,
+//     MatStepperModule,
+//     MatDatepickerModule,
+//     MatDialogModule,
+//     MatDividerModule,
+//     MatExpansionModule,
+//     MatGridListModule,
+//     MatIconModule,
+//     MatInputModule,
+//     MatListModule,
+//     MatMenuModule,
+//     MatNativeDateModule,
+//     MatPaginatorModule,
+//     MatProgressBarModule,
+//     MatProgressSpinnerModule,
+//     MatRadioModule,
+//     MatRippleModule,
+//     MatSelectModule,
+//     MatSidenavModule,
+//     MatSliderModule,
+//     MatSlideToggleModule,
+//     MatSnackBarModule,
+//     MatSortModule,
+//     MatTableModule,
+//     MatTabsModule,
+//     MatToolbarModule,
+//     MatTooltipModule,
 //   ]
 // })
-
-
-import {BrowserModule} from '@angular/platform-browser';
-import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
-import {
-  MatAutocompleteModule,
-  MatButtonModule,
-  MatButtonToggleModule,
-  MatCardModule,
-  MatCheckboxModule,
-  MatChipsModule,
-  MatDatepickerModule,
-  MatDialogModule,
-  MatDividerModule,
-  MatExpansionModule,
-  MatGridListModule,
-  MatIconModule,
-  MatInputModule,
-  MatListModule,
-  MatMenuModule,
-  MatNativeDateModule,
-  MatPaginatorModule,
-  MatProgressBarModule,
-  MatProgressSpinnerModule,
-  MatRadioModule,
-  MatRippleModule,
-  MatSelectModule,
-  MatSidenavModule,
-  MatSliderModule,
-  MatSlideToggleModule,
-  MatSnackBarModule,
-  MatSortModule,
-  MatStepperModule,
-  MatTableModule,
-  MatTabsModule,
-  MatToolbarModule,
-  MatTooltipModule,
-} from '@angular/material';
-
-@NgModule({
-  exports: [
-    MatAutocompleteModule,
-    MatButtonModule,
-    MatButtonToggleModule,
-    MatCardModule,
-    MatCheckboxModule,
-    MatChipsModule,
-    MatStepperModule,
-    MatDatepickerModule,
-    MatDialogModule,
-    MatDividerModule,
-    MatExpansionModule,
-    MatGridListModule,
-    MatIconModule,
-    MatInputModule,
-    MatListModule,
-    MatMenuModule,
-    MatNativeDateModule,
-    MatPaginatorModule,
-    MatProgressBarModule,
-    MatProgressSpinnerModule,
-    MatRadioModule,
-    MatRippleModule,
-    MatSelectModule,
-    MatSidenavModule,
-    MatSliderModule,
-    MatSlideToggleModule,
-    MatSnackBarModule,
-    MatSortModule,
-    MatTableModule,
-    MatTabsModule,
-    MatToolbarModule,
-    MatTooltipModule,
-  ]
-})
-export class MaterialModule { }
+// export class MaterialModule { }
